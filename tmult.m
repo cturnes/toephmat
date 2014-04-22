35cd407a1c30189d62aff744be45c5a35a92fb73
cturnes <cturnes@users.noreply.github.com>
2014-04-22 18:58:19 -0400

    Update tmult.m

--- tmult.m
+++ tmult.m
@@ -1,19 +1,17 @@
-function y = tmult(a, x, p)
+function y = tmult(a, x)
 % TMULT  Toeplitz matrix multiplication
 %
 %   Y = TMULT(A, X) will return the multiplication of the M x N Toeplitz
 %   matrix T, whose coefficients are defined by the (M+N-1) x 1 vector A,
 %   with the N x 1 vector X.
 %
-%   Y = TMULT(A, X, P) will do the same, using a maximum FFT prime factor
-%   of P (default = inf).
 %
 %       See also toep
 %
 %   Christopher K. Turnes
 %   Georgia Institute of Technology
 %   Version:  2.0.0
-%   Date:     1-Oct-2013
+%   Date:     22-Apr-2014
 %
 %
 
@@ -26,35 +24,15 @@ function y = tmult(a, x, p)
     n = size(x, 1);
     m = length(a) - n + 1;
     if (m < 0)
-        error('MATLAB:tmultfcn:innerDimMismatch', ...
+        error('MATLAB:tmult:innerDimMismatch', ...
             'Inner dimmensions do not match');
     end
     
-    % check if min prime factor is supplied, start convolution
-    if (nargin < 3)
-        A = cfft(a, m+2*n -2);
-    else
-        
-        if (isempty(p))
-            warning('MATLAB:tmult:noPrimeBound', ...
-                'Third argument is empty; setting default prime bound to 5');
-            p = 5;
-        else
-            if (length(p) > 1)
-                warning('MATLAB:tmult:primeBoundNotScalar', ...
-                    'Prime bound must be integer scalar greater than 1.');
-                p = p(1);
-            end
-            if (p < 2)
-                error('MATLAB:tmult:primeBoundTooSmall', ...
-                    'Prime bound must be integer scalar greater than 1.');
-            end
-        end
-        A = cfft(a, m+2*n-2, p);
-    end
+    % start convolution
+    A = fft(a, m+2*n -2);
     
     % perform convolution
-    y = ifft(cfft(x, length(A)) .* A(:, ones(1, size(x, 2))));
+    y = ifft(fft(x, length(A)) .* A(:, ones(1, size(x, 2))));
     
     % truncate
     y = y(n:(n+m-1), :);
